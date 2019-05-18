1cf3562d052a96ee637c1594990696cb5bcbd1c5
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2019-05-18 10:44:00 -0300

    feature(Hooks): finaliza exemplo de projetos do github com react hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  return <h1>Hello</h1>;
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.github.com/users/fellipegpbotelho/repos"
+      );
+      const data = await response.json();
+      setRepositories(data);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `Você tem ${filtered.length} favoritos :)`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+    setRepositories(newRepositories);
+  }
+
+  return (
+    <div>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            <button onClick={() => handleFavorite(repo.id)}>
+              {repo.favorite ? "Desfavoritar" : "Favoritar"}
+            </button>
+            <b style={{ marginLeft: 10 }}>{repo.name}</b>
+            {repo.favorite && <span>(Favorito)</span>}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
